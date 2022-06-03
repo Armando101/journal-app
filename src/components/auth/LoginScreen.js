8cde1c8da8ae4edcfc0e8968503910445f9aa851
Armando Rivera <rivera.armando997@gmail.com>
2022-06-03 15:21:08 -0500

    redux - configure redux and initial state

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,24 +1,44 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { login } from "../../actions/auth";
+import { useForm } from "../../hooks/useForm";
 
 export const LoginScreen = () => {
+  const dispatch = useDispatch();
+
+  const [formValues, handleInputChange] = useForm({
+    email: "rivera.armando997@gmail.com",
+    password: "123456",
+  });
+
+  const { email, password } = formValues;
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(login("1234", "Armando"));
+  };
+
   return (
     <>
       <h3 className="auth__title">Login</h3>
-      <form>
+      <form onSubmit={handleLogin}>
         <input
           className="auth__input"
           autoComplete="off"
           type="text"
+          onChange={handleInputChange}
           placeholder="Email"
           name="email"
+          value={email}
         />
         <input
           className="auth__input"
           autoComplete="off"
           type="password"
+          onChange={handleInputChange}
           placeholder="Password"
           name="password"
+          value={password}
         />
 
         <button className="btn btn-primary btn-block" type="submit">
